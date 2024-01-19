34020c9ae0ac2dfda8300de69141a4ccc935e809
Kim Jintae <realbig419@naver.com>
2024-01-19 17:00:58 +0900

    캔버스 기능 추가 및 수정

--- public/script.js
+++ public/script.js
@@ -1,29 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
 
   socket.on("connected", (data) => {
     console.log(data);
   });
 
-  //   // HTML 요소 내부에서의 움직임을 감지함
-  //   document.body.onmousemove = (event) => {
-  //     const messageBody = {
-  //       x: event.clientX,
-  //       y: event.clientY,
-  //     };
-  //     socket.emit("mousemove", messageBody);
-  //   };
-
-  // 화면 전체에서의 움직임을 감지함
-  document.addEventListener("mousemove", function (event) {
-    const messageBody = {
-      x: event.clientX,
-      y: event.clientY,
-    };
+  canvas.onmousemove = (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const messageBody = { x, y };
     socket.emit("mousemove", messageBody);
-  });
+
+    // 캔버스에 마우스 위치를 그립니다.
+    drawOnCanvas(ctx, x, y);
+  };
 
   socket.on("message", (data) => {
     console.log("Message from server:", data);
   });
 });
+
+// function drawOnCanvas(ctx, x, y) {
+//   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+//   ctx.fillStyle = "red";
+//   ctx.beginPath();
+//   ctx.arc(x, y, 10, 0, 2 * Math.PI);
+//   ctx.fill();
+// }
+
+function drawOnCanvas(ctx, x, y) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "blue";
+  ctx.font = "20px Arial";
+  ctx.fillText("여기!", x, y);
+}
